1777fc4f2d74012473d551bcbdbfcac1911edb6c
Leon Ebel <24588023+eberureon@users.noreply.github.com>
2024-02-26 14:39:24 +0100

    update nvim config

--- .config/nvim/lua/config/plugins/lsp.lua
+++ .config/nvim/lua/config/plugins/lsp.lua
@@ -30,7 +30,9 @@ return {
       local builtin = require('telescope.builtin')
 
       nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-      nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+      nmap('<leader>ca', function()
+        vim.lsp.buf.code_action { context = { only = { 'quickfix', 'refactor', 'source' } } }
+      end, '[C]ode [A]ction')
 
       nmap('gd', builtin.lsp_definitions, '[G]oto [D]efinition')
       nmap('gr', builtin.lsp_references, '[G]oto [R]eferences')

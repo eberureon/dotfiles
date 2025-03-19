f3711bfe86ed376ff0af44ce2d91774c1ad7fbff
ebelleon <24588023+eberureon@users.noreply.github.com>
2025-03-19 08:45:50 +0100

    add linux zsh config, update wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -7,6 +7,9 @@ local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
 
+config.initial_cols = 170
+config.initial_rows = 40
+
 -- my coolnight colorscheme
 config.colors = {
   foreground = "#869395",
@@ -21,10 +24,10 @@ config.colors = {
 }
 
 config.font = wezterm.font("PragmataPro Mono")
-config.font_size = 14
+config.font_size = 12
 
 config.window_decorations = "RESIZE"
--- config.window_background_opacity = 0.75
+config.window_background_opacity = 0.9
 -- config.macos_window_background_blur = 10
 
 config.keys = {

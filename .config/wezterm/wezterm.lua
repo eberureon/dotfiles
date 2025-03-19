f69a6de3b74fd299cd7c5c7cde7b650b0a5176df
ebelleon <24588023+eberureon@users.noreply.github.com>
2025-03-19 09:05:52 +0100

    darken blue

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -10,6 +10,7 @@ local config = wezterm.config_builder()
 config.initial_cols = 170
 config.initial_rows = 40
 
+-- config.color_scheme = "Tokyo Night (Gogh)"
 -- my coolnight colorscheme
 config.colors = {
   foreground = "#869395",
@@ -19,15 +20,15 @@ config.colors = {
   cursor_fg = "#011423",
   selection_bg = "#163540",
   selection_fg = "#CBE0F0",
-  ansi = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#0FC5ED", "#a277ff", "#24EAF7", "#24EAF7" },
-  brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
+  ansi = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#0DADD1", "#A277FF", "#24EAF7", "#24EAF7" },
+  brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#A277FF", "#24EAF7", "#24EAF7" },
 }
 
 config.font = wezterm.font("PragmataPro Mono")
 config.font_size = 12
 
 config.window_decorations = "RESIZE"
-config.window_background_opacity = 0.9
+-- config.window_background_opacity = 0.9
 -- config.macos_window_background_blur = 10
 
 config.keys = {

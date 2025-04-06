ce86ac1dcce8b3048bf2c9492d7e81001169931f
Leon Ebel <24588023+eberureon@users.noreply.github.com>
2025-04-06 09:47:26 +0200

    add new plugins neovim, update ghostty

--- .config/lazyvim/init.lua
+++ .config/lazyvim/init.lua
@@ -1,2 +1,7 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
+if vim.g.vscode then
+  -- VSCode extension
+  require("config.lazy")
+else
+  -- bootstrap lazy.nvim, LazyVim and your plugins
+  require("config.lazy")
+end
